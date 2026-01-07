218ca2dda976af1339f409e1567dc103e5d36b03
Skill Glider <skillglider4@gmail.com>
2026-01-07 21:43:39 +0530

    Delete const supabaseUrl = 'https: directory

--- const supabaseUrl = 'https:/superbase.js
+++
@@ -1,8 +0,0 @@
-<script type="module">
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-  
-const supabaseUrl = 'https://qataihohmogsjgtdnarz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhdGFpaG9obW9nc2pndGRuYXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3ODk1NzAsImV4cCI6MjA4MzM2NTU3MH0.F6nqNwnBZA5AQDBNtQNcW9AHd16QUFib9jBshKNGIKk';
-
-window.supabase = createClient(supabaseUrl, supabaseAnonKey);
-</script>
